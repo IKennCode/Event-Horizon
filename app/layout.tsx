7ab570a8ba868ec8d3088471200da4352eef8c23
IKennCode <dimalibotkennethofficial@gmail.com>
2024-02-01 22:01:56 +0800

    sdsad

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: 'Event Oasis',
   description: 'Event Oasis is a platform for event management.',
   icons: {
-    icon: '/assets/images/logo.png'
+    icon: '/assets/images/logo.svg'
   }
 }
 
